d853d83080a4b3534f9a6328aab5da4b3467052c
nirajnale <132734839+nirajnale@users.noreply.github.com>
2026-04-21 19:39:39 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def insert(self, intervals, newInterval):
+        res = []
+        i = 0
+        n = len(intervals)
+
+        # Step 1: add left intervals
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        # Step 2: merge overlaps
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        res.append(newInterval)
+
+        # Step 3: add remaining
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
