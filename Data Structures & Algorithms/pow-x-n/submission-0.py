0e3d5525c0d72a6c53fde21949fa7baedf7a052a
nirajnale <132734839+nirajnale@users.noreply.github.com>
2026-04-25 18:46:16 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x, n):
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        
+        half = self.myPow(x, n // 2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
+            
